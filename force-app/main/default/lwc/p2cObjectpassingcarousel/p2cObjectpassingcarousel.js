9ecbb4b4c9093397d827767a11331210a42142af
Vasanth <gvkp38@gmail.com>
2025-07-27 08:57:29 +0530

    parent to child communication

---
+++ force-app/main/default/lwc/p2cObjectpassingcarousel/p2cObjectpassingcarousel.js
@@ -0,0 +1,5 @@
+import { LightningElement,api } from 'lwc';
+
+export default class P2cObjectpassingcarousel extends LightningElement {
+    @api carouselobject;
+}
---
+++ force-app/main/default/lwc/p2cPrimitiveparent/p2cPrimitiveparent.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class P2cPrimitiveparent extends LightningElement {
+    value
+    carouselobjectdata=[
+        {'src':"https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",'header': "First Card",'description': "First card description.",'alternative-text':"First card accessible description."},
+        {'src':"https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",'header': "Second Card",'description': "Second card description.",'alternative-text':"second card accessible description."},
+        {'src':"https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",'header': "Third Card",'description': "Third card description.",'alternative-text':"third card accessible description."}]
+        handlechange(event)
+        {
+            this.value=event.target.value;
+        }
+}
