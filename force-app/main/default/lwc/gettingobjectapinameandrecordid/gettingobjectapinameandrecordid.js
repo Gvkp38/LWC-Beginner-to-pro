88b93d479c2569d11bc3eb2d89febe223bb2a42c
Vasanth <gvkp38@gmail.com>
2025-08-22 17:01:04 +0530

    useridfromuserapi

--- force-app/main/default/lwc/gettingobjectapinameandrecordid/gettingobjectapinameandrecordid.js
+++ force-app/main/default/lwc/gettingobjectapinameandrecordid/gettingobjectapinameandrecordid.js
@@ -1,6 +1,7 @@
 import { api,LightningElement } from 'lwc';
-
+import Id from '@salesforce/user/Id';
 export default class Gettingobjectapinameandrecordid extends LightningElement {
     @api recordId;
-    @api objectApiName
+    @api objectApiName;
+    userId=Id
 }
