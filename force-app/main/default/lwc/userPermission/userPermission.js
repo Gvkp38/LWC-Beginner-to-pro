76a85626e5c6d38e79fd1aefee0120c484a3dc57
Vasanth <gvkp38@gmail.com>
2025-08-23 12:45:32 +0530

    Addingcustompermission

--- force-app/main/default/lwc/userPermission/userPermission.js
+++ force-app/main/default/lwc/userPermission/userPermission.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 import hasViewAllData from "@salesforce/userPermission/ViewAllData";
-
+import HASCUSTPER from "@salesforce/customPermission/Viewcasecustompermission";
 export default class UserPermission extends LightningElement {
     hasViewAllData = hasViewAllData;
+    Hascustompermission=HASCUSTPER;
+
 }
