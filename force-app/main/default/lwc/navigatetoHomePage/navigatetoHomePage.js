a44d735c6214c1d82c67c3b54708ec030cd88844
Vasanth <gvkp38@gmail.com>
2025-08-26 15:55:41 +0530

    navigate to standard__namedPage

---
+++ force-app/main/default/lwc/navigatetoHomePage/navigatetoHomePage.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigatetoHomePage extends NavigationMixin(LightningElement) {
+navigatetohome()
+{
+    this[NavigationMixin.Navigate](
+        {
+            type: 'standard__namedPage',
+    attributes: { pageName: 'home' }
+        }
+    )
+}
+navigatetochatter()
+{
+    this[NavigationMixin.Navigate](
+        {
+            type: 'standard__namedPage',
+    attributes: { pageName: 'chatter' }
+        }
+    )
+}
+
+}
